8ee5b85b991223cbd8f4a25bab85f9f8f088c0f5
winton <dc1021@ic.ac.uk>
2025-07-13 18:34:41 +0100

    fixed entrypoint

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'drive = drive_nodes:drive.main'
+            'drive = drive_nodes.drive:main'
         ],
     },
 )
